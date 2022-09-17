64e3794c80d29f1377aa7b37de9cb40994672c68
Joe Barker <joe.joeb29@gmail.com>
2022-09-17 14:03:58 +0300

    Working on cmdln

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 class Main{
     companion object {
         @JvmStatic fun main(args: Array<String>) {
+            val input = generateSequence(::readLine)
+            val lines = input.toList()
+            run(lines, args[0].split(":"))
+        }
+
+        private fun run(config: List<String>, time: List<String>)
+        {
+            val converter = ConfigConverter(time[0], time[1])
+            val results = converter.convert(config)
+            println("\n")
+            results.forEach{ println(it) }
         }
     }
 }
